6f57c261779b1190a78925bccf8a212b99f3c84d
Santosh <Santosh@DESKTOP-4CSHG8D>
2020-01-03 22:26:47 +0530

    Modified Logger code

--- src/test/java/com/epam/automation/tests/Test1.java
+++ src/test/java/com/epam/automation/tests/Test1.java
@@ -16,31 +16,20 @@ import com.epam.automation.pages.StartPage;
 import com.epam.automation.util.AutomationUtil;
 
 public class Test1 {
-	
-	private static final Logger LOGGER =  
-            Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
-    //private final String USERNAME = "testautomationuser";
-    //private final String PASSWORD = "Time4Death!";
 
-    @Test
-    public void testOneCanLoginGithub() {
-    	try {
-			AutomationUtil.loadProperties();
-		} catch (FileNotFoundException e) {
-			LOGGER.info(e.getMessage());
-		} catch (IOException e) {
-			LOGGER.info(e.getMessage());
-		}
-    	final String USERNAME = AutomationUtil.getUserName();
-    	final String PASSWORD = AutomationUtil.getPassword();
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        StartPage startPage = new StartPage(driver);
-        startPage.open();
-        SignInPage signInPage = startPage.invokeSignIn();
-        HomePage homePage = signInPage.signIn(USERNAME, PASSWORD);
-        String loggedInUserName = homePage.getLoggedInUserName();
-        Assert.assertEquals(USERNAME, loggedInUserName);
-        driver.quit();
-    }
+	@Test
+	public void testOneCanLoginGithub() {
+		AutomationUtil.loadProperties();
+		final String USERNAME = AutomationUtil.getUserName();
+		final String PASSWORD = AutomationUtil.getPassword();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		StartPage startPage = new StartPage(driver);
+		startPage.open();
+		SignInPage signInPage = startPage.invokeSignIn();
+		HomePage homePage = signInPage.signIn(USERNAME, PASSWORD);
+		String loggedInUserName = homePage.getLoggedInUserName();
+		Assert.assertEquals(USERNAME, loggedInUserName);
+		driver.quit();
+	}
 }
--- src/test/java/com/epam/automation/util/AutomationUtil.java
+++ src/test/java/com/epam/automation/util/AutomationUtil.java
@@ -5,17 +5,26 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
+import java.util.logging.Logger;
 
 public class AutomationUtil {
 	
+	private static final Logger LOGGER =  
+            Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 	private AutomationUtil() {
 		
 	}
 	static Properties prop;
 	
-	public static void loadProperties() throws FileNotFoundException, IOException {
+	public static void loadProperties() {
 		prop =  new Properties();
-		prop.load(new FileInputStream(new File("properties.properties")));
+		try {
+			prop.load(new FileInputStream(new File("properties.properties")));
+		} catch (FileNotFoundException e) {
+			LOGGER.info(e.getMessage());
+		} catch (IOException e) {
+			LOGGER.info(e.getMessage());
+		}
 	}
 	
 	public static String getURL() {
